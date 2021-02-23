49e4a8e27d88630342f24a3cbbf21515e4dfe69e
Maciej Warakomski <Warakom247365@gmail.com>
2021-02-23 21:17:22 +0100

    Initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        GuessNumber();
+    }
+
+    void GuessNumber()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        if (max > min)
+        {
+            min = guess + 1;
+        }
+        GuessNumber();
+    }
+
+    public void OnPressLower()
+    {
+        if (max > min)
+        {
+            max = guess - 1;
+        }
+        GuessNumber();
+    }
+}
